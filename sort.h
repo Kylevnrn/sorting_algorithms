0e4fcf65a3467e6639808b0641b941370911a98d
Kylevnrn <kylevnrn@gmail.com>
2023-01-24 11:52:58 -0800

    Task 0

--- sort.h
+++ sort.h
@@ -20,10 +20,13 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
+
+/* print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-listint_t *swap_node(listint_t *node, listint_t **list);
+
+/* sorting algorithms */
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
